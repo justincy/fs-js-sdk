9375040045cf30fefee09809a441e0b7a4629762
Justin York <justincyork@gmail.com>
2013-02-06 00:47:00 -0700

    Minor tweaks to usage

--- app.js
+++
@@ -1,8 +0,0 @@
-var fs = require('./lib/fs.js');
-fs = new fs('');
-
-fs.getDiscovery(function(response1){
-  fs.getDiscovery(function(response2){
-    console.log( response1 == response2 );
-  });
-});
